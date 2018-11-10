b77ebda46c26a816a0613b07d2d3eaa4da9e8cdf
MAVArce <marquinhos_213@hotmail.com>
2018-11-10 00:34:39 -0200

    Finished not commented

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-#define OK 0
 #define ERROR 1
 
 struct node{
@@ -19,7 +17,6 @@ NODE* create_door(char entry[]){
     NODE* new;
     
     new=(NODE*)calloc(1, sizeof(NODE));
-    new->bit=-1;
     new->left=NULL;
     new->right=NULL;
     strcpy(new->name, entry);
@@ -27,7 +24,63 @@ NODE* create_door(char entry[]){
     return new;
 }
 
-void create_system(NODE** root){
+void search(NODE** root, NODE** cursor, char entry[]){
+    if((*cursor)!=NULL)
+        return;
+
+    if(strcmp(entry, (*root)->name)==0){
+        *cursor=*root;
+        return;
+    }
+    
+    if((*root)->name[0]=='E')
+        return;
+    
+    if((*root)->left!=NULL){
+        search(&(*root)->left, cursor, entry);
+    }
+
+    if((*root)->name[0]=='N')
+        return;
+    
+    if((*root)->right!=NULL){
+        search(&(*root)->right, cursor, entry);
+    }
+}
+
+void create_system_a(NODE** root){
+    NODE* cursor = NULL;
+    char entry[4];  //String que receberá do buffer se o nó é uma porta ou uma entrada e seu índice
+    char entry_l[4];
+    char entry_r[4];
+
+    scanf("%s", entry);
+    entry[3]='\0';
+    getchar();
+
+    if(*root==NULL){
+        *root=create_door(entry);
+        cursor=*root;
+    }else{
+        search(root, &cursor , entry);
+        if(cursor==NULL)    
+            return;
+    }
+    //printf("I get here\n");
+    scanf("%s", entry_l);
+    entry_l[3]='\0';
+    getchar();
+    cursor->left=create_door(entry_l);
+
+    if(cursor->name[0]!='N'){
+        scanf("%s", entry_r);
+        entry_r[3]='\0';
+        getchar();
+        cursor->right=create_door(entry_r);
+    }
+}
+
+void create_system_b(NODE** root){
     char entry[4];  //String que receberá do buffer se o nó é uma porta ou uma entrada e seu índice
     
     scanf("%s", entry);
@@ -39,16 +92,16 @@ void create_system(NODE** root){
     if((*root)->name[0]=='E')   
         return;
 
-    create_system(&(*root)->left);
+    create_system_b(&(*root)->left);
     if((*root)->name[0]=='N'){
         return;
     }
-    create_system(&(*root)->right);
+    create_system_b(&(*root)->right);
 }
 
 void insert_bites(NODE** root){
     if((*root)->name[0]=='E'){
-        (*root)->bit=getchar();
+        scanf("%d", &(*root)->bit);
         getchar();
         return;
     }
@@ -64,65 +117,83 @@ int operations(NODE* root){
     char type;
     int left;
     int right;
+    int result;
 
     type=root->name[0];
     
-    if(root->left!=NULL){
+    if(root->left!=NULL)
         left=operations(root->left);
-    }
-    if(root->right!=NULL){
+    if(root->right!=NULL)
         right=operations(root->right);
+    
+    if(type=='E'){
+        result = root->bit;
+    }else if(type=='A'){
+        result = left & right;
+    }else if(type=='O'){
+        result = left | right;
+    }else if(type=='D'){
+        result = (~(left & right) & 1);
+    }else if(type=='R'){
+        result = (~(left | right) & 1);
+    }else if(type=='X'){
+        result = left ^ right;
+    }else if(type=='N'){
+        result = (~left & 1);
+    }
+    return result;
+}
+
+void free_all(NODE** root){
+    if((*root)->name[0]=='E'){
+        free(*root);
+        *root=NULL;
+        return;
     }
 
-    if(type=='E'){
-        return root->bit;
-    }else if(type=='A'){
-        return left & right;
-    }else if(type=='O'){
-        return left | right;
-    }else if(type=='D'){
-        return ~(left & right);
-    }else if(type=='R'){
-        return ~(left | right);
-    }else if(type=='X'){
-        return left ^ right;
-    }else if(type=='N'){
-        return ~left;
+    free_all(&(*root)->left);
+    if((*root)->name[0]=='N'){
+        free(*root);
+        *root=NULL;
+        return;
+    }
+    free_all(&(*root)->right);
+    
+    if((*root)->left==NULL && (*root)->right==NULL){
+        free(*root);
+        *root=NULL;
+        return;
     }
 }
 
 int main (int argc, char* argv[]) {
-    NODE* root;     //Ponteiro de uma variável node que servirá como uma raiz da árvore
+    NODE* root = NULL;     //Ponteiro de uma variável node que servirá como uma raiz da árvore
     int mode;       //Inteiro que conterá o modo em que será introduzido o sistema
     int lines;      //Número de linhas para o caso 3-a
     int loops;      //Número de vezes em que serão inseridas entradas diferentes
-
-    root=(NODE*)calloc(1, sizeof(NODE));
-    root->left=NULL;
-    root->right=NULL;
-    
-    if(root==NULL){
-        printf("Erro na alocação");
-        return ERROR;
-    }
 
     scanf("%d", &mode);
     getchar();
     
     if(mode==0){
-
+        scanf("%d", &lines);
+        getchar();
+        
+        for( ; lines>0; lines--){
+            create_system_a(&root);
+        }
     }else if(mode==1){
-        create_system(&root);
+        create_system_b(&root);
     }
 
     scanf("%d", &loops);
     getchar();
 
-    for(; loops>0; loops--){
+    for( ; loops>0; loops--){
         insert_bites(&root);
         printf("%d\n", operations(root));
     }
 
-    free(root);
+    free_all(&root);
     return 0;
 }
